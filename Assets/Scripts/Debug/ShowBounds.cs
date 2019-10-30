f7898d5494a8f17b53b03a872595165be6d0abb2
pumpcrin <shacsi_tr1835@yahoo.com>
2019-10-30 10:17:43 +0900

    20191030

---
+++ Assets/Scripts/Debug/ShowBounds.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+
+public class DebugTools : MonoBehaviour{
+    public static DebugTools Instance;
+
+    List<DebugToolElement> toolInstances;
+
+    void Start(){
+        if(Instance == null)
+            Instance = this;
+        
+        toolInstances = new List<DebugToolElement>();
+        toolInstances.Add(new DebugToolElement(new ShowBounds()));
+    }
+
+    public void MethodInvoke(Type type){
+        var toolElem = toolInstances.Where(elem => elem.GetType() == type).First();
+        
+    }
+}
+
+public class DebugToolElement{
+    public DebugToolElement(IDebugTool _instance){
+        instance = _instance;
+    }
+
+    public bool isUsed = false;
+    public IDebugTool instance;
+}
+
+public interface IDebugTool{
+    void SetUp();
+    void Method(object[] arguments);
+}
+
+public class ShowBounds : IDebugTool
+{
+    const string assetPath = "Assets/Materials/Debug/BoundsCube.mat";
+
+    public bool isUsed = false;
+    Material boundsCubeMaterial;
+
+    public void SetUp(){
+        boundsCubeMaterial = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+        isUsed = true;
+    }
+
+    public void Method(object[] arguments){
+        CreateBoundsCube((Vector3)arguments[0], (Quaternion)arguments[1], (Vector3)arguments[2]);
+    }
+
+    public void CreateBoundsCube(Vector3 pos, Quaternion rot, Vector3 sca){
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.GetComponent<Renderer>().material = boundsCubeMaterial;
+        cube.transform.localScale = sca;
+        cube.transform.rotation = rot;
+        cube.transform.position = pos;
+
+    }
+}
--- Assets/Scripts/VillageBuilder.cs
+++ Assets/Scripts/VillageBuilder.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class VillageBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject house;
+    public GameObject houseInstance_debug;
+    public Material boundsCubeMaterial_debug;
+
     void Start()
     {
-        
+        var bounds = house.GetComponent<MeshFilter>().sharedMesh.bounds;
+        Debug.Log("bounds"+bounds);
+
+        var boundsCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        boundsCube.transform.localScale = bounds.extents * 2;
+        boundsCube.transform.rotation = houseInstance_debug.transform.rotation;
+        boundsCube.transform.position = houseInstance_debug.transform.position + bounds.center;
+        boundsCube.GetComponent<Renderer>().material = boundsCubeMaterial_debug;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
